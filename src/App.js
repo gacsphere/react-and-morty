e6975188bee9bbe8b29c97bce42f2bb3f61b1a57
Gesa Siebert <hallo@gesasiebert.de>
2022-09-30 10:19:24 +0200

    add styling to card component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import "./App.css";
+// import "./App.css";
 
 import Header from "./components/header/Header";
-import Card from ".components/card/Card";
+import Card from "./components/card/Card";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-      <main></main>
+      <Main>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Main>
       <nav>
         <button>1</button>
         <button>2</button>
         <button>3</button>
         <button>4</button>
       </nav>
-    </div>
+    </>
   );
 }
+
+const Main = styled.main`
+  // background-color: #fff4e1;
+  // background-color: #cef3ff;
+  padding: 6em 0;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,13 +6,14 @@ function Header() {
 
 const HeaderStyled = styled.h1`
   text-align: center;
+  font-size: 2em;
   z-index: 20;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   background-color: deepskyblue;
-  //   border-bottom: 1px solid black;
+  // border-bottom: 1px solid black;
   padding: 8px;
   color: white;
 `;
