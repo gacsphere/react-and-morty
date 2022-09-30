28c0a3068c2111598b077772ea103257d4068796
Gesa Siebert <hallo@gesasiebert.de>
2022-09-30 11:36:46 +0200

    style Navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,41 @@
 import styled from "styled-components";
+// import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-    </nav>
+    <NavStyled>
+      <NavButton>HOME</NavButton>
+      <NavButton>RANDOM</NavButton>
+      <NavButton>FAVORITES</NavButton>
+      <NavButton>GET CREATIVE</NavButton>
+    </NavStyled>
   );
 }
+
+const NavStyled = styled.nav`
+  background-color: deepskyblue;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+`;
+
+const NavButton = styled.button`
+  background: transparent;
+  border: none;
+  color: white;
+  padding: 1em;
+  width: 100%;
+  font-weight: 600;
+  font-size: 1em;
+  &:hover {
+    cursor: pointer;
+    background-color: #00a7df;
+  }
+  &:active {
+    background-color: #00a7df;
+  }
+`;
 
 export default Navigation;
